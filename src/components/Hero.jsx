8f65d5ebb55346082ba599ccb52fe1ffdaef1efd
Md Irfan <mdirfanansari413@gmail.com>
2025-11-04 11:51:54 +0530

    Services Section Completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import assets from '../assets/assets'
 
+
 const Hero = () => {
   return (
     <div id='hero' className='flex flex-col items-center gap-6 py-20 px-4 sm:px-12 lg:px-24 xl:px-40 text-center w-full overflow-hidden text-gray-700 '>
