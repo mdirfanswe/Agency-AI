445724b420569d140145e376f81c29d7eaae10ac
Md Irfan <mdirfanansari413@gmail.com>
2025-11-06 12:42:17 +0530

    Animation added at Services section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import assets from '../assets/assets'
 import Title from './Title'
 import ServiceCard from './ServiceCard'
+import { motion } from "motion/react"
 
 const Services = () => {
 
@@ -31,7 +32,13 @@ const Services = () => {
     },
   ]
   return (
-    <div id='services' className='relative flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'>
+    <motion.div 
+        initial="hidden"
+        whileInView="visible"
+        viewport={{once: true}} //! This if for display the animation only once 
+        transition={{staggerChildren: 0.2}}
+        
+    id='services' className='relative flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'>
 
       <img src={assets.bgImage2} alt="" className='absolute -top-110 -left-70 -z-1 dark:hidden' />
 
@@ -43,7 +50,7 @@ const Services = () => {
         ))}
       </div>
 
-    </div>
+    </motion.div>
   )
 }
 
